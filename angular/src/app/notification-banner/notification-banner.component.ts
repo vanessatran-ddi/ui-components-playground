72038b985e85e1ffd4aa0414be6e472d538e5310
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-04-11 14:30:44 -0600

    fix banner for modal drawer
    Signed-off-by: Vanessa Tran <vanessa.m.tran@gov.ab.ca>

--- angular/src/app/notification-banner/notification-banner.component.ts
+++ angular/src/app/notification-banner/notification-banner.component.ts
@@ -1,4 +1,4 @@
-import { GoabNotificationBanner } from "@abgov/angular-components";
+import { GoabNotification } from "@abgov/angular-components";
 import { Component } from "@angular/core";
 
 @Component({
@@ -6,7 +6,7 @@ import { Component } from "@angular/core";
   selector: "abgov-notification-banner",
   templateUrl: "./notification-banner.component.html",
   imports: [
-    GoabNotificationBanner,
+    GoabNotification,
   ]
 })
 export class NotificationBannerComponent {
