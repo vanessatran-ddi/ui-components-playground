f92a7bb1581d715f02bd526287f973cda7ced98c
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-05-01 16:56:57 -0600

    add bug 2409
    Signed-off-by: Vanessa Tran <vanessa.m.tran@gov.ab.ca>

---
+++ angular/src/app/issues/2409/issue-2409.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { GoabFormItem, GoabInput } from "@abgov/angular-components";
+@Component({
+  selector: "abgov-issue-2409",
+  templateUrl: "./issue-2409.component.html",
+  imports: [GoabFormItem, GoabInput],
+  standalone: true,
+})
+export class Issue2409Component {}
