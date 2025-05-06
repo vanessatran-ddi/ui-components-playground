fd049c70507bf90d502206e7361ad5826f77f1e1
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-05-06 17:03:54 -0600

    update code for 2693
    Signed-off-by: Vanessa Tran <vanessa.m.tran@gov.ab.ca>

---
+++ angular/src/app/issues/2693/issue-2693.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostListener } from "@angular/core";
+import { GoabButton, GoabIconButton, GoabPopover } from "@abgov/angular-components";
+@Component({
+  selector: "abgov-issue-2693",
+  standalone: true,
+  imports: [GoabPopover, GoabButton, GoabIconButton],
+  templateUrl: "./issue-2693.component.html",
+})
+export class Issue2693Component {
+  // In your component .ts file
+  args = { key1: 'value1', key2: 'value2' };
+  @HostListener('window:delete-user', ['$event'])
+  onDeleteUser(event: any) {
+    console.log("event is ", event);
+    const actionData = event.detail;
+    this.handleDeleteUser(actionData);
+  }
+
+  private handleDeleteUser(data: any) {
+    alert("Delete is clicked" + JSON.stringify(data))
+  }
+}
