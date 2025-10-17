c26a238efc626b0386acedac65c8040f57f8bc87
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-10-17 13:06:11 -0600

    add some code
    Signed-off-by: Vanessa Tran <vanessa.m.tran@gov.ab.ca>

--- react/src/app/app.tsx
+++ react/src/app/app.tsx
@@ -20,46 +20,48 @@ export function App() {
         </GoabAppHeader>
       </section>
       <section className="content">
-        <DataGrid/>
-        {/*<section className="side-menu">*/}
-        {/*  <GoabSideMenu>*/}
-        {/*    <GoabSideMenuGroup heading="Bugs">*/}
-        {/*      <Link to="/2446">Popover inside a scrolling modal</Link>*/}
-        {/*      <Link to="/2408">Form stepper status</Link>*/}
-        {/*      <Link to="/2441">Issue 2441</Link>*/}
-        {/*      <Link to="/1216">Issue 1216</Link>*/}
-        {/*      <Link to="/2404">2404 Angular Input TrailingIcon Fix</Link>*/}
-        {/*      <Link to="/1769">1769 autocomplete property</Link>*/}
-        {/*      <Link to="/2768">2768 Radio dynamic disabled state</Link>*/}
-        {/*      <Link to="/2772">2772 align on input</Link>*/}
-        {/*      <Link to="/2789">2789 width rem percentage</Link>*/}
-        {/*      <Link to="/2829">2829 modal focus fix</Link>*/}
-        {/*      <Link to="/2547">2574 Banner hides pop over safari</Link>*/}
-        {/*      <Link to="/2720">2720 Change tab via link</Link>*/}
-        {/*    </GoabSideMenuGroup>*/}
-        {/*    <GoabSideMenuGroup heading="Components">*/}
-        {/*      <Link to="/">Home</Link>*/}
-        {/*      <Link to="/pagination">Pagination</Link>*/}
-        {/*      <Link to="/drawer">Drawer</Link>*/}
-        {/*      <Link to="/temporary-notification">Temporary Notification</Link>*/}
-        {/*      <Link to="/Text">Text</Link>*/}
-        {/*      <Link to="/data-grid">Data Grid</Link>*/}
+        <section className="side-menu">
+          <GoabSideMenu>
+            <GoabSideMenuGroup heading="Bugs">
+              <Link to="/2446">Popover inside a scrolling modal</Link>
+              <Link to="/2408">Form stepper status</Link>
+              <Link to="/2441">Issue 2441</Link>
+              <Link to="/1216">Issue 1216</Link>
+              <Link to="/2404">2404 Angular Input TrailingIcon Fix</Link>
+              <Link to="/1769">1769 autocomplete property</Link>
+              <Link to="/2768">2768 Radio dynamic disabled state</Link>
+              <Link to="/2772">2772 align on input</Link>
+              <Link to="/2789">2789 width rem percentage</Link>
+              <Link to="/2829">2829 modal focus fix</Link>
+              <Link to="/2547">2574 Banner hides pop over safari</Link>
+              <Link to="/2720">2720 Change tab via link</Link>
+            </GoabSideMenuGroup>
+            <GoabSideMenuGroup heading="Components">
+              <Link to="/">Home</Link>
+              <Link to="/pagination">Pagination</Link>
+              <Link to="/drawer">Drawer</Link>
+              <Link to="/temporary-notification">Temporary Notification</Link>
+              <Link to="/Text">Text</Link>
+              <Link to="/data-grid">Data Grid</Link>
 
-        {/*      /!* Add links here *!/*/}
-        {/*    </GoabSideMenuGroup>*/}
-        {/*    <GoabSideMenuGroup heading={"Public Form"}>*/}
-        {/*      <Link to="/public-form">Public Form</Link>*/}
-        {/*      <Link to="/public-form-navigation">Navigation Test (Issue 1)</Link>*/}
-        {/*      <Link to="/public-form-accessibility">Accessibility Test (Issue 2)</Link>*/}
-        {/*      <Link to="/2827">Issue 2827</Link>*/}
-        {/*      <Link to="/pr2969-test">PR 2969 Test Form</Link>*/}
-        {/*    </GoabSideMenuGroup>*/}
-        {/*    /!* Add links here *!/*/}
-        {/*  </GoabSideMenu>*/}
-        {/*</section>*/}
-        {/*<main className="main">*/}
-        {/*  <Outlet />*/}
-        {/*</main>*/}
+              {/* Add links here */}
+            </GoabSideMenuGroup>
+            <GoabSideMenuGroup heading={"Public Form"}>
+              <Link to="/public-form">Public Form</Link>
+              <Link to="/public-form-navigation">Navigation Test (Issue 1)</Link>
+              <Link to="/public-form-accessibility">Accessibility Test (Issue 2)</Link>
+              <Link to="/2827">Issue 2827</Link>
+              <Link to="/pr2969-test">PR 2969 Test Form</Link>
+            </GoabSideMenuGroup>
+            <GoabSideMenuGroup heading={"Workspace"}>
+              <Link to={"/workspace-search-page"}>Search page</Link>
+            </GoabSideMenuGroup>
+            {/* Add links here */}
+          </GoabSideMenu>
+        </section>
+        <main className="main">
+          <Outlet />
+        </main>
       </section>
       <section slot="footer">
         <GoabAppFooter />
--- react/src/main.tsx
+++ react/src/main.tsx
@@ -27,6 +27,7 @@ import { Issue2827 } from "./app/public-form/Issue2827";
 import { Issue2547 } from "./app/issues/issue2547";
 import { PR2969TestForm } from "./app/public-form/PR2969TestForm";
 import { DataGrid } from "./app/components/data-grid/DataGrid";
+import { SearchPage } from "./app/workspace/SearchPage";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
@@ -60,6 +61,7 @@ root.render(
           <Route path={"/2547"} element={<Issue2547/>}></Route>
           <Route path={"/pr2969-test"} element={<PR2969TestForm/>}></Route>
           <Route path={"/data-grid"} element={<DataGrid/>}></Route>
+          <Route path={"/workspace-search-page"} element={<SearchPage/>}></Route>
           {/** Add more routes here */}
 
         </Route>
