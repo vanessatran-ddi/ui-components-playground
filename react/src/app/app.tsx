b64be4b69719d1bb83b95ae7ef0c2a0116cdb0dc
Vanessa Tran <vanessa.m.tran@gov.ab.ca>
2025-10-21 08:01:42 -0600

    move workspace to main page
    Signed-off-by: Vanessa Tran <vanessa.m.tran@gov.ab.ca>

--- react/src/app/app.tsx
+++ react/src/app/app.tsx
@@ -9,6 +9,7 @@ import {
   GoabSideMenuGroup,
   GoabSideMenuHeading,
 } from "@abgov/react-components";
+import { WorkSpaceSideMenu } from "./workspace/WorkSpaceSideMenu";
 
 export function App() {
   return (
@@ -86,6 +87,77 @@ export function App() {
         <GoabAppFooter />
       </section>
     </GoabOneColumnLayout>
+    <WorkSpaceSideMenu/>
+    // <GoabOneColumnLayout>
+    //   <section slot="header">
+    //     <GoabMicrositeHeader type="alpha" version="UAT" />
+    //     <GoabAppHeader url="/" heading="Design System">
+    //       <a href="/login">Sign in</a>
+    //     </GoabAppHeader>
+    //   </section>
+    //   <section className="content">
+    //     <section className="side-menu">
+    //       <GoabSideMenu>
+    //         <GoabSideMenuHeading>Bugs</GoabSideMenuHeading>
+    //           <GoabSideMenuGroup heading={"Modal"}>
+    //             <Link to="/2446">Popover inside a scrolling modal</Link>
+    //             <Link to="/2829">2829 modal focus fix</Link>
+    //             <Link to="/2948">2948 Modal Spacing</Link>
+    //           </GoabSideMenuGroup>
+    //           <GoabSideMenuGroup heading={"Form stepper"}>
+    //             <Link to="/2408">Form stepper status</Link>
+    //           </GoabSideMenuGroup>
+    //           <GoabSideMenuGroup heading={"Dropdown"}>
+    //             <Link to="/2441">Issue 2441</Link>
+    //             <Link to="/2789">2789 width rem percentage</Link>
+    //             <Link to="/2934">2934 Value types (string|number|boolean)</Link>
+    //           </GoabSideMenuGroup>
+    //           <GoabSideMenuGroup heading={"Checkbox/Radio Reveal"}>
+    //             <Link to="/1216">Issue 1216</Link>
+    //             <Link to="/2768">2768 Radio dynamic disabled state</Link>
+    //             <Link to="/3001">3001 Checkbox dispatch string</Link>
+    //           </GoabSideMenuGroup>
+    //           <GoabSideMenuGroup heading={"Input"}>
+    //             <Link to="/2404">2404 Angular Input TrailingIcon Fix</Link>
+    //             <Link to="/1769">1769 autocomplete property</Link>
+    //             <Link to="/2772">2772 align on input</Link>
+    //           </GoabSideMenuGroup>
+    //           <GoabSideMenuGroup heading={"Popover"}>
+    //             <Link to="/2547">2574 Banner hides pop over safari</Link>
+    //           </GoabSideMenuGroup>
+    //           <GoabSideMenuGroup heading={"Tabs"}>
+    //             <Link to="/2720">2720 Change tab via link</Link>
+    //           </GoabSideMenuGroup>
+    //         <GoabSideMenuHeading>Components</GoabSideMenuHeading>
+    //         <Link to="/pagination">Pagination</Link>
+    //         <Link to="/drawer">Drawer</Link>
+    //         <Link to="/temporary-notification">Temporary Notification</Link>
+    //         <Link to="/Text">Text</Link>
+    //         <Link to="/menu-button">Menu Button</Link>
+    //         <Link to="/checkbox-list">Checkbox List</Link>
+    //         <GoabSideMenuGroup heading={"Public Form"}>
+    //           <Link to="/public-form">Public Form</Link>
+    //           <Link to="/public-form-navigation">Navigation Test (Issue 1)</Link>
+    //           <Link to="/public-form-accessibility">Accessibility Test (Issue 2)</Link>
+    //           <Link to="/2827">Issue 2827</Link>
+    //           <Link to="/public-form-checkbox-list">Public form with checkbox list</Link>
+    //           <Link to="/pr2969-test">PR 2969 Test Form</Link>
+    //         </GoabSideMenuGroup>
+    //         <GoabSideMenuGroup heading="WorkSpace">
+    //           <Link to={"/workspace-side-menu"}>Menu</Link>
+    //         </GoabSideMenuGroup>
+    //         {/* Add links here */}
+    //       </GoabSideMenu>
+    //     </section>
+    //     <main className="main">
+    //       <Outlet />
+    //
+    //     </main>
+    //   </section>
+    //   <section slot="footer">
+    //     <GoabAppFooter />
+    //   </section>
+    // </GoabOneColumnLayout>
   );
 }
 
